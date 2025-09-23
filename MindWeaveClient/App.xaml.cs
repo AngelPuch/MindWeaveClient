6bf52de6c4a63dbe2fce071928e51981dc5e691a
Angel Puch <angeljonathanpuchhernandez@gmail.com>
2025-09-23 11:16:34 -0600

    Implementación de ventanas

--- MindWeaveClient/App.xaml.cs
+++ MindWeaveClient/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading;
 using System.Windows;
 
 namespace MindWeaveClient
@@ -13,5 +8,11 @@ namespace MindWeaveClient
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var langCode = MindWeaveClient.Properties.Settings.Default.languageCode;
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(langCode);
+            base.OnStartup(e);
+        }
     }
 }
---
+++ MindWeaveClient/View/Authentication/AuthenticationWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MindWeaveClient.View.Authentication
+{
+    /// <summary>
+    /// Lógica de interacción para AuthenticationWindow.xaml
+    /// </summary>
+    public partial class AuthenticationWindow : Window
+    {
+        public AuthenticationWindow()
+        {
+            InitializeComponent();
+            AuthenticationFrame.Navigate(new CreateAccountPage());
+        }
+    }
+}
