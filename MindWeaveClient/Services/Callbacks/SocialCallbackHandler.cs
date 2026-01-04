40a2d0043d5bea66654ca9f10b6f584dab66e9f9
hyelo <antoniocgomez02@gmail.com>
2026-01-04 16:20:38 -0600

    solucionar problemas en los metodos sincronos en esperar de solicitudes de amistad

--- MindWeaveClient/Services/Callbacks/SocialCallbackHandler.cs
+++ MindWeaveClient/Services/Callbacks/SocialCallbackHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ServiceModel;
+using System.Windows.Threading;
 using System.Windows;
 using MindWeaveClient.SocialManagerService;
 
@@ -15,34 +16,34 @@ namespace MindWeaveClient.Services.Callbacks
 
         public void notifyFriendRequest(string fromUsername)
         {
-            Application.Current.Dispatcher.Invoke(() =>
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
             {
                 FriendRequestReceived?.Invoke(fromUsername);
-            });
+            }));
         }
 
         public void notifyFriendResponse(string fromUsername, bool accepted)
         {
-            Application.Current.Dispatcher.Invoke(() =>
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
             {
                 FriendResponseReceived?.Invoke(fromUsername, accepted);
-            });
+            }));
         }
 
         public void notifyFriendStatusChanged(string friendUsername, bool isOnline)
         {
-            Application.Current.Dispatcher.Invoke(() =>
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
             {
                 FriendStatusChanged?.Invoke(friendUsername, isOnline);
-            });
+            }));
         }
 
         public void notifyLobbyInvite(string fromUsername, string lobbyId)
         {
-            Application.Current.Dispatcher.Invoke(() =>
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
             {
                 LobbyInviteReceived?.Invoke(fromUsername, lobbyId);
-            });
+            }));
         }
 
     }
